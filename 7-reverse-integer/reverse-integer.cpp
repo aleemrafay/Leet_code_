2a88c83f5273a9b93bdd505fd8c927f491bf0e97
Abdul Rafay Aleem <aleemrafay.1@gmail.com>
2026-08-03 17:56:10 +0500

    Time: 3 ms (29.42%) | Memory: 8.5 MB (54.84%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.cpp
@@ -0,0 +1,18 @@
+//aleem_rafay_
+class Solution {
+public:
+    int reverse(int x) {
+        int sum = 0;
+
+        while (x != 0) {
+            int temp = x % 10;
+            x /= 10;
+            if (sum > INT_MAX / 10 || (sum == INT_MAX / 10 && temp > 7)) return 0;
+            if (sum < INT_MIN / 10 || (sum == INT_MIN / 10 && temp < -8)) return 0;
+
+            sum = sum * 10 + temp;
+        }
+
+        return sum;
+    }
+};
