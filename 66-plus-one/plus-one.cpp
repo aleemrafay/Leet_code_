5867b52a4fce45fa73ceaa2a26f2f679dcd06fd7
Abdul Rafay Aleem <aleemrafay.1@gmail.com>
2026-08-17 01:56:41 +0500

    Time: 0 ms (100.00%) | Memory: 11.6 MB (27.06%) - LeetSync

---
+++ 66-plus-one/plus-one.cpp
@@ -0,0 +1,18 @@
+//aleem_rafay
+class Solution {
+public:
+    std::vector<int> plusOne(std::vector<int>& digits) {
+        int n = digits.size();
+        
+        for (int i = n - 1; i >= 0; --i) {
+            if (digits[i] < 9) {
+                digits[i] += 1;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        
+         digits.insert(digits.begin(), 1);
+        return digits;
+    }
+};
