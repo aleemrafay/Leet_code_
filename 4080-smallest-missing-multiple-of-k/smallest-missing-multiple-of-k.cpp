f69e04d746fe59b536f6ad187b3cb643a75c085a
Abdul Rafay Aleem <aleemrafay.1@gmail.com>
2026-08-25 15:38:38 +0500

    Time: 3 ms (38.06%) | Memory: 25.1 MB (45.06%) - LeetSync

---
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.cpp
@@ -0,0 +1,14 @@
+//aleem_rafay
+class Solution {
+public:
+    int missingMultiple(std::vector<int>& nums, int k) {
+        std::unordered_set<int> seen(nums.begin(), nums.end());
+        
+        int multiple = k;
+        while (seen.count(multiple)) {
+            multiple += k;
+        }
+        
+        return multiple;
+    }
+};
